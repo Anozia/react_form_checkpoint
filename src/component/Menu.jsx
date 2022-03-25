155ad65b6c7235cc3a1b927dcc3fca1387b5fdb7
Kelechi Anozia <kelechianozia@gmail.com>
2022-03-25 21:47:28 +0100

    added changes

---
+++ src/component/Menu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+
+const Menu = () => {
+  return (
+    <div>
+      <menu />
+
+      <leftSide />
+
+      <rightSide />
+    </div>
+  );
+};
+
+export default Menu;

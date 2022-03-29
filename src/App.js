cd9a6c248ccc6fb11aafd1b2b48a1f9acbcc86de
Kelechi Anozia <kelechianozia@gmail.com>
2022-03-29 10:00:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import {
   FormControl,
   Button,
   Row,
-  Col,n
+  Col,
+  n,
 } from "react-bootstrap";
 
 import "./component/Menu";
@@ -88,32 +89,6 @@ function App() {
       </div>
     </div>
   );
-
-  // function App() {
-  //   return (
-  //     <div className="Form">
-  //       <Form>
-  //         <Form.Group className="mb-3" controlId="formBasicEmail">
-  //           <Form.Label>Email address</Form.Label>
-  //           <Form.Control type="email" placeholder="Enter email" />
-  //           <Form.Text className="text-muted">
-  //             We'll never share your email with anyone else.
-  //           </Form.Text>
-  //         </Form.Group>
-
-  //         <Form.Group className="mb-3" controlId="formBasicPassword">
-  //           <Form.Label>Password</Form.Label>
-  //           <Form.Control type="password" placeholder="Password" />
-  //         </Form.Group>
-  //         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-  //           <Form.Check type="checkbox" label="Check me out" />
-  //         </Form.Group>
-  //         <Button variant="primary" type="submit">
-  //           Submit
-  //         </Button>
-  //       </Form>
-  //     </div>
-  //     );
 }
 
 export default App;
